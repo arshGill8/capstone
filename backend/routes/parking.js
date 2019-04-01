7f4f48e3e004a2d73042cacebf2eae3660a710b0
Arsh <arsh_8008@hotmail.com>
2019-03-31 22:23:45 -0400

    front end fix

--- backend/routes/parking.js
+++ backend/routes/parking.js
@@ -4,14 +4,7 @@ const parkingData = require('../parkingData'),
       express = require('express'),
       router = express.Router();
 
-    // let spot = parkingData.map(parking => parking.parkingspot) 
-    // let newlat = parkingData.map(parking => parking.lat)
-    // let newlng = parkingData.map(parking => parking.lng)
-
-    // function between (lat,lng,newlat,newlng){
-    //     if (lat)
-    // }
-
+  
     router.post('/location', (req, res) => {
         let {lat, lng} = req.body
         let parkarray = parkingData.map(parkinglot => {
